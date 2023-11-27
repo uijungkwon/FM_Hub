f6a933fb3f9e34bdcd6e13a0c42167e2a1ae2338
uijung kwon <kuj08036410@gmail.com>
2023-11-27 21:52:15 +0900

    파일 이름 변경

--- fm_hub_uijung/src/main/java/com/example/fm_hub_uijung/model/MovieListModel.kt
+++ fm_hub_uijung/src/main/java/com/example/fm_hub_uijung/model/MovieListModel.kt
@@ -1,8 +1,5 @@
 package com.example.fm_hub_uijung.model
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
 class MovieListModel {
     var movieList: MutableList<MovieInfo>? = null
 }
