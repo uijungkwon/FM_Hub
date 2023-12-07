b4535c67aa12807891a43dc33b898e88635c738c
dazzang22 <144760153+dazzang22@users.noreply.github.com>
2023-12-07 14:48:35 +0900

    //~

--- app/src/main/java/com/example/fm_hub/MainActivity.kt
+++ app/src/main/java/com/example/fm_hub/MainActivity.kt
@@ -7,19 +7,18 @@ import android.os.Build
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 
 import com.example.fm_hub.databinding.ActivityAuthBinding
+import com.example.fm_hub.databinding.ActivityMainBinding
 import com.example.fm_hub.databinding.NavigationHeaderBinding
 import com.example.fm_hub.firebase.AuthActivity
 import com.example.fm_hub.firebase.MyFirebaseApplication
 
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.fm_hub.databinding.ActivityMainBinding
 import com.example.fm_hub.model.GeoLocation
 import com.example.fm_hub.model.Quad
 
@@ -30,7 +29,7 @@ import kotlinx.coroutines.launch
 import java.util.Calendar
 
 
-// 코루틴 데이터 공유를 위한 뷰모델 추가
+// 코루틴 데이터 공유를 위한 뷰모델 추가~
 object CinemaDataStorage {
     var geoLocationList: ArrayList<Quad<GeoLocation, String, String, String>>? = null
 }
@@ -43,10 +42,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     lateinit var headerView: View
     lateinit var loginBtn: TextView
     lateinit var loginUser: TextView
-    // 메인화면 recyclerview
-    lateinit var mRecyclerView: RecyclerView
-    lateinit var mRecyclerAdapter: MyRecyclerAdapter
-    lateinit var mfriendItems: ArrayList<FriendItem>
+
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -93,42 +89,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             // 메인 액티비티에서 데이터 저장
             CinemaDataStorage.geoLocationList = geoLocationList
         }
-        // recyclerview by yeongsinkeem
-        mRecyclerView = binding.recyclerViewMovie
-        mRecyclerAdapter = MyRecyclerAdapter()
-
-        mRecyclerView.adapter = mRecyclerAdapter
-        mRecyclerView.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        mfriendItems = ArrayList()
-        for (i in 1..3) {
-            mfriendItems.add(
-                FriendItem(
-                    R.drawable.movie1,
-                    "하이큐!! 승자와 패자",
-                    "예매율 87.9%",
-                    "누적관객 122,176명, 당일관객 53,379명"
-                )
-            )
-            mfriendItems.add(
-                FriendItem(
-                    R.drawable.movie2,
-                    "Call me by your name",
-                    "예매율 43.3%",
-                    "누적관객 210,496명, 당일관객 23,321명"
-                )
-            )
-            mfriendItems.add(
-                FriendItem(
-                    R.drawable.movie3,
-                    "그대들은 어떻게 살 것인가",
-                    "예매율 40.1%",
-                    "누적관객 170,883명, 당일관객 10,230명"
-                )
-            )
-        }
-        mRecyclerAdapter.setFriendList(mfriendItems);
     }
 
     /*로그인, 로그아웃 상태에 따라 다르게 "View" 보여주기*/
